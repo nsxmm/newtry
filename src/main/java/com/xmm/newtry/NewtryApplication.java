f268d04b7feefaf782104dbe51b36fa06445ef51
你是小明明啊 <714711549@qq.com>
2024-03-20 12:55:54 +0800

    init

---
+++ src/main/java/com/xmm/newtry/NewtryApplication.java
@@ -0,0 +1,13 @@
+package com.xmm.newtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewtryApplication.class, args);
+    }
+
+}
